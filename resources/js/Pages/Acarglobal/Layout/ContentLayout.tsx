bc54be08e4eb39f058fd22250efb0843f1ba1b50
thanhnt <buitienthanhnt@gmail.com>
2026-05-06 00:12:41 +0700

    acar-dev: Update code

--- resources/js/Pages/Acarglobal/Layout/ContentLayout.tsx
+++ resources/js/Pages/Acarglobal/Layout/ContentLayout.tsx
@@ -6,7 +6,7 @@ export default function ContentLayout({ children }: { children: React.ReactNode 
   return (
     <div className="flex flex-col flex-1 min-h-screen w-full">
       {children}
-      <Link href={'/acar/'}>
+      <Link className="no-print z-10" href={'/acar/'}>
         <HomeIcon className="no-print size-10 absolute bottom-10 right-10 text-gray-500 hover:text-gray-900"></HomeIcon></Link>
     </div>
   )
